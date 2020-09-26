85d7a3372397a44d5ecc58f76babb0d65cc19a29
Saul Maldonado <samaldonado4@gmail.com>
2020-09-26 03:33:39 -0500

    test: add jest and eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,20 @@ module.exports = {
   env: {
     es2021: true,
     node: true,
+    'jest/globals': true,
   },
   extends: ['airbnb-base', 'prettier'],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
+  plugins: ['jest'],
   rules: {
     'no-console': 0,
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
   },
 };
